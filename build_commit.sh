3dfb4e676d3f0762fa6fed27c3a29c7f62ac4aa6
kowsik.s <kowsikaugust02@gmail.com>
2020-01-04 18:38:50 +0530

    basic build config

---
+++ build_commit.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+( cp -R ../quizdevelopment  ./  && echo "local commit successfull" ) ||     echo "fail to copy"
+
+
---
+++ build_update.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+
+( cp -R ./quizdevelopment  ../  && echo "local updated successfully" ) ||     echo "fail to copy"
+
+
